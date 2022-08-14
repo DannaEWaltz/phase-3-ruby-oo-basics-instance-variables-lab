45628157ab17d2e5ae595667f13f71f78f56ab3a
+ <+dannaewaltz@gmail.com>
2022-08-14 09:51:58 -0600

    commit tree 53111795ab6e934d40024738340f4f32361f6d19

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-
+    #setting or assigning value
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    #getting or reporting the individual dog name
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
+
+
+
+
